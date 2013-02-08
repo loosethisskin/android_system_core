696ec92690477bb27faae993383473e3ec38566c
William Roberts <w.roberts@sta.samsung.com>
2013-02-08 09:41:26 +0900

    Reserve AID_AUDIT
    AID_AUDIT will be used for an audit daemon for gathering
    and controlling the Linux audit subsystem.
    
    Change-Id: I95d597524b5547a60f3a59f692b4b0a6df0a9645
    
    Conflicts:
    
    	include/private/android_filesystem_config.h
    
    Conflicts:
    	include/private/android_filesystem_config.h

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -78,6 +78,7 @@
 #define AID_SDCARD_ALL    1035  /* access all users external storage */
 #define AID_LOGD          1036  /* log daemon */
 #define AID_SHARED_RELRO  1037  /* creator of shared GNU RELRO files */
+#define AID_AUDIT         1038  /* audit daemon */
 
 #define AID_SHELL         2000  /* adb and debug shell user */
 #define AID_CACHE         2001  /* cache access */
@@ -171,6 +172,7 @@ static const struct android_id_info android_ids[] = {
 
     { "misc",          AID_MISC, },
     { "nobody",        AID_NOBODY, },
+    { "audit",      AID_AUDIT, },
 };
 
 #define android_id_count \
